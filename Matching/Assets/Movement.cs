8f80179610d6d608935aaff6f814b8973649dfec
JCG <jcgg92@gmail.com>
2017-04-24 17:20:56 +0200

    Flappy Stuff
    Ya flappea Sosio

---
+++ Matching/Assets/Movement.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	public InputManager IM;
+
+	[Header ("Gravity")]
+	public float grav;
+	public float gravStart;
+	public float gravIncrement;
+	public float maxGrav;
+
+	[Header ("Speed")]
+	public float speed;
+	public float speedStart;
+	public float speedIncrement;
+	public float maxSpeed;
+
+
+	void Start (){
+		speed = speedStart;
+		grav = gravStart;
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+		ApplyGrav ();
+
+		if (grav < maxGrav)
+			AddGrav ();
+
+		if (IM.inputPress) {
+			ApplySpeed ();
+			AddSpeed ();
+		} else
+			speed = speedStart;
+	}
+
+	void AddGrav () {
+		grav += gravIncrement;
+	}
+
+	void ApplyGrav () {
+		transform.position = transform.position - new Vector3 (0, grav, 0);
+	}
+
+	public void AddSpeed () {
+		if (speed < maxSpeed)
+			speed += speedIncrement;
+	}
+
+	public void ApplySpeed () {
+		grav = gravStart;
+		transform.position = transform.position + new Vector3 (0, speed, 0);
+	}
+
+}
---
+++ Matching/Assets/VisualMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisualMovement : MonoBehaviour {
+
+	public float lerpSpeed;
+	public Transform parent;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position = Vector3.Lerp (transform.position, parent.position, lerpSpeed);
+	}
+}
