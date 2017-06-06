b8b6dc30238e10c1aad9b178486704b85e75e916
Aleix-PC\Aleix <aleixmixmax@hotmail.com>
2017-06-06 02:33:28 +0200

    StartScene Aleix

---
+++ Matching/Assets/StartMenuScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class StartMenuScript : MonoBehaviour {
+
+
+    public string leveltoload = "Escena";
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Play();
+        }
+    }
+
+    public void Play()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // la escena actual del buildindex
+        SceneManager.LoadScene(leveltoload);
+
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+
+
+
+
+}
